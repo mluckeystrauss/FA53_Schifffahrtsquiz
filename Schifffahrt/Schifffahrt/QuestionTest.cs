16f18a902f5481582f38ef09468ec3755a3c4ac6
kai.mo <kai.moerker@web.de>
2017-03-21 08:46:58 +0100

    install NUnit and add test case for question

---
+++ Schifffahrt/Schifffahrt/QuestionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Schifffahrt
+{
+    [TestFixture]
+    class QuestionTest
+    {
+        [Test]
+        public void IsTextCorrect()
+        {
+            Question q = new Question("Wie heißt die Hauptstadt Deutschlands?",
+                                        new List<Answer>());
+
+            string result = q.Text;
+
+            Assert.AreEqual(result, "Wie heißt die Hauptstadt Deutschlands?");
+        }
+    }
+}
