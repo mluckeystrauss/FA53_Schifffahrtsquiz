464df7ae7ccb8b1952cfb82a661801d3b36d53af
kai.mo <kai.moerker@web.de>
2017-03-21 10:16:51 +0100

    add test for correctness of answer list

--- Schifffahrt/Schifffahrt/QuestionTest.cs
+++ Schifffahrt/Schifffahrt/QuestionTest.cs
@@ -11,15 +11,34 @@ namespace Schifffahrt
     [TestFixture]
     class QuestionTest
     {
+        Question q;
+        List<Answer> answers;
+
+        [SetUp]
+        public void SetupQuestion()
+        {
+            answers = new List<Answer>();
+
+            answers.Add( new Answer("Berlin") );
+            answers.Add( new Answer("Madrid") );
+            answers.Add( new Answer("Rom") );
+            answers.Add( new Answer("Castrop-Rauxel") );
+
+            q = new Question(5, "Wie heißt die Hauptstadt Deutschlands?", answers);
+        }
+
+
         [Test]
         public void ArePropertiesCorrect()
         {
             Question q = new Question(5, "Wie heißt die Hauptstadt Deutschlands?",
-                                        new List<Answer>());
+                                        answers );
 
             Assert.AreEqual(q.Text, "Wie heißt die Hauptstadt Deutschlands?");
-
             Assert.AreEqual(q.ID, 5);
+            Assert.AreEqual(q.Answers, answers);
+
+            Assert.IsFalse(q.Is_Answered);
         }
     }
 }
